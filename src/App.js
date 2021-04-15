451b665707e8d75f2ac1c5320f4a7ddf1591b8c7
chkue2 <chkue0924@gmail.com>
2021-04-15 10:58:59 +0900

    create

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
+import Button from './components/Button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Buttons">
+        <Button size="large">BUTTON</Button>
+        <Button>BUTTON</Button>
+        <Button size="small">BUTTON</Button>
+      </div>
+      <div className="Buttons">
+        <Button color="gray" size="large">BUTTON</Button>
+        <Button color="gray">BUTTON</Button>
+        <Button color="gray" size="small">BUTTON</Button>
+      </div>
+      <div className="Buttons">
+        <Button color="pink" size="large">BUTTON</Button>
+        <Button color="pink">BUTTON</Button>
+        <Button color="pink" size="small">BUTTON</Button>
+      </div>
+      <div className="Buttons">
+        <Button size="large" outline>BUTTON</Button>
+        <Button color="gray" outline>BUTTON</Button>
+        <Button color="pink" size="small" outline>BUTTON</Button>
+      </div>
+      <div className="Buttons">
+        <Button size="large" fullWith className="customized-button">BUTTON</Button>
+        <Button color="gray" size="large" fullWith>BUTTON</Button>
+        <Button color="pink" size="large" fullWith 
+        onClick={() => {console.log('click');}}
+        onMouseMove={() => {console.log('move');}}>BUTTON</Button>
+      </div>
     </div>
+
   );
 }
 
